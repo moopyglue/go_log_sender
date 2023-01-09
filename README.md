6c33ec10067836b4f26ded0858a1bec382a5ebaa
Simon Wall <moopyglue@gmail.com>
2023-01-09 15:12:51 +0000

    added basic README content and simplified the test log creation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go_log_sender
+
+Used to tail a log file and send it to a remote server.
+
+Designed to run on Windows client and Unix/Windows server.
+
+Simon Wall
--- logmaker/main.go
+++ logmaker/main.go
@@ -7,60 +7,28 @@ import (
 	"math/rand"
 	"os"
 	"time"
-
-	"github.com/natefinch/lumberjack"
 )
 
 func main() {
 
 	flag.Parse()
-	args := flag.Args()
-
-	log.SetOutput(&lumberjack.Logger{
-		Filename:   args[0],
-		MaxBackups: 4,
-		MaxSize:    1,
-	})
-
-	currentTime := time.Now()
-	logid := fmt.Sprintf(
-		"%s-%s-%s-%d",
-		currentTime.Format("20060102-150405"),
-		os.Getenv("USERNAME"),
-		os.Getenv("COMPUTERNAME"),
-		os.Getpid(),
-	)
-
-	log.Printf("logid: %s\n", logid)
-	log.Printf("Hello from Go application!\n")
-
 	rand.Seed(time.Now().UnixNano())
 
-	linecount := 2
-	padding := "882y4t8njfgiej"
-	tmp := ""
+	padding := "88ijyn5498yun89u89uc5ythju4yth78gy8fjuy458ky9v8ek8y56y7i4ythk7v457kyn7k4vt0u450umnv845u85vun805y0c,4xi5is395iv9nbnyu0v6598uldifu408p6yu35v9numih,d4w9,uyw508uybv8m6;ucd4luymml049mvl0u4yb95uy49lpcul48u8oyunv3o8luo82uc45yu48u5y8,54uyp96uykn0o078olk0pt,i6np.vumjocms2y4t8njfgiej"
+	linecount := 0
 
-	// generate 12-72 log lines, with 1-10 times padding string
-	// this then pauses 30ms to avoid CPU soaking
+	f, err := os.OpenFile(flag.Args()[0], os.O_CREATE|os.O_APPEND, 755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+
 	for {
-		log.SetOutput(&lumberjack.Logger{
-			Filename:   args[0],
-			MaxBackups: 0,
-			MaxSize:    100,
-		})
-
 		rand2 := int(rand.Intn(50) + 5)
-
 		for i := 1; i <= rand2; i++ {
 			linecount++
-			rand1 := int(rand.Intn(10) + 1)
-			tmp = fmt.Sprintf("linecount %d [%d] ", linecount, rand1)
-			for j := 1; j <= rand1; j++ {
-				tmp += padding
-			}
-			// write to log rolled outout using
-			// lumberjack module
-			log.Println(tmp)
+			rand1 := int(rand.Intn(100) + 20)
+			log.Printf("linecount %d [%03d] %s", linecount, rand1, padding[0:rand1])
 		}
 		fmt.Printf("linecount=%d\n", linecount)
 		time.Sleep(1000 * time.Millisecond)
